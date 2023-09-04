b595ae29dcd2fda42329a81be27e2c827063eed9
asif <mohammedasifraza.2020cs@technonjr.org>
2023-09-04 18:20:26 +0530

    add file to trdc

--- Prob1.cpp
+++ Prob1.cpp
@@ -1,39 +1,27 @@
-#include <iostream>
-using namespace std;
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        int total_gas = 0;
+        int current_gas = 0;
+        int start_gas = 0;
 
-int main() {
-    int n;
-    cin >> n;
-    int gas[n];
-    int cost[n];
+        for (int i = 0; i < n; i++) {
+            total_gas += gas[i] - cost[i];
+            current_gas += gas[i] - cost[i];
 
-    for (int i = 0; i < n; i++) {
-        cin >> gas[i];
-    }
+            if (current_gas < 0) {
+                start_gas = i + 1;
+                current_gas = 0;
+            }
+        }
 
-    for (int i = 0; i < n; i++) {
-        cin >> cost[i];
-    }
-
-    int total_gas = 0;
-    int current_gas = 0;
-    int start_gas = 0;
-
-    for (int i = 0; i < n; i++) {
-        total_gas += gas[i] - cost[i];
-        current_gas += gas[i] - cost[i];
-
-        if (current_gas < 0) {
-            start_gas = i + 1;
-            current_gas = 0;
+        if (total_gas >= 0) {
+            return start_gas;
+        } else {
+            return -1;
         }
     }
 
-    if (total_gas >= 0) {
-        cout<< start_gas << endl;
-    } else {
-        cout<<-1<< endl;
-    }
-
-    return 0;
-}
+        
+};
